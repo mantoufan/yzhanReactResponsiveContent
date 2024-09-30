1df17d3bf7c93408791d63dbcb7e08a0da6a76ba
mantoufan <mhjlw@126.com>
2024-09-30 20:00:38 +0800

    chore: update the dist to docs

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "https://github.com/mantoufan/yzhanReactResponsiveContent",
+  build: {
+    outDir: path.resolve(__dirname, "../docs"),
+    emptyOutDir: true,
+  },
 });
